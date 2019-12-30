0c4e9bab56cc24aca6405642d04cb3cc2da68347
Uanderson Nunes de Lima <uandersonmbc@hotmail.com>
2019-12-30 14:01:26 -0300

    Melhorado os teste de componentes

--- src/__test__/CSelect.test.js
+++ src/__test__/CSelect.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { mount, configure } from 'enzyme'
+import { mount, configure, shallow } from 'enzyme'
 
 import Adapter from 'enzyme-adapter-react-16'
 
@@ -9,23 +9,27 @@ import CSelect from '../components/CSelect';
 configure({ adapter: new Adapter() })
 
 describe('CSelect components', () => {
-
-    it('Testing component properties', () => {
+    let component;
+    const sexs = [
+        { value: 'MALE', description: 'MALE' },
+        { value: 'FEMALE', description: 'FEMALE' }
+    ]
+    beforeEach(() => {
         const sexs = [
             { value: 'MALE', description: 'MALE' },
             { value: 'FEMALE', description: 'FEMALE' }
         ]
-        const component = mount(
+        component = shallow(
             <CSelect
                 onChange={() => jest.fn()}
                 placeholder="Select a gender"
                 options={sexs}
             />
-        )
+        );
+    })
 
+    test('checking the amount of options', () => {
+        expect(component.find('Option').length).toBe(2)
         expect(component.props().placeholder).toEqual("Select a gender")
-        expect(component.props().options).toEqual(sexs)
-
-        component.unmount()
     })
 });
